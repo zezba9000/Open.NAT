4a2a36e031d97f3dc806fc149e2a36d5408ed094
lontivero <lucasontivero@gmail.com>
2016-08-30 00:32:06 -0300

    license, authors & ut

---
+++ Open.Nat.UnitTests/UpnpNatDeviceInfoTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Open.Nat.UnitTests
+{
+	[TestFixture]
+	public class UpnpNatDeviceInfoTests
+	{
+		[Test]
+		public void x()
+		{
+			var info = new UpnpNatDeviceInfo(IPAddress.Loopback, new Uri("http://127.0.0.1:3221"), "/control?WANIPConnection", null);
+			Assert.AreEqual("http://127.0.0.1:3221/control?WANIPConnection", info.ServiceControlUri.ToString());
+		}
+	}
+}
--- Open.Nat/AssemblyInfo.cs
+++ Open.Nat/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Open.Nat")]
@@ -15,3 +16,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyFileVersion("2.1.0.0")]
 [assembly: CLSCompliant(false)]
+[assembly: InternalsVisibleTo("Open.Nat.UnitTests,PublicKey=" + 
+	"00240000048000009400000006020000002400005253413100040000010001004de491ec95076f"+
+	"7054ac0562395232c4c351bce7037c7e266a5cc5dc6c60738fe649e06513ca1d58edec8e6a60e9"+
+	"69887403a09ae158a007a592f650fe98912e7029e112e61494a47eff16138b62390241e1d8d450"+
+	"76ac3af77b47fa67d40ba1928a37ebe073415785a23ae87b6119f63f78f04cb3b8ea280dfbf3d6"+
+	"df437fbf")]
